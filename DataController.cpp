cf85dde5bea9669a6c9856fbb09b9668cc1716ed
Kazuma-Ogasa <knct0831@gmail.com>
2022-06-26 22:51:26 +0900

    add comments to database processors

--- DataController.cpp
+++ DataController.cpp
@@ -30,59 +30,68 @@
 
     int Update(const char* dataFileName, const char* gid, const char* type, char* value){
 
-            FILE *fp_write;
+            FILE *database;
             
             //r+ は読み書き両方のモード
-            fp_write = fopen(dataFileName, "r+");
+            database = fopen(dataFileName, "r+");
 
             while(1){
 
-                //id読み取り用配列（一応毎回初期化する)    
+                //id読み取り用配列（一応毎回初期化する)
+                //終端文字分のプラス 1    
                 char id[SIZE_GID + 1];
                 memset(id, '\0', sizeof(id));
 
                 //ファイルからGIDを取得
                 for(int i = 0; i < SIZE_GID; i++){
-                    id[i] = fgetc(fp_write);
+                    // 1文字ずつ読み取り
+                    id[i] = fgetc(database);
 
                 //終了判定 データベースの最終行は「end」の文字列
                 //ゴリ押しif文感満載
                     if(id[0] == 'e' && id[1] == 'n' && id[2] == 'd'){
                         printf("end");
-                        return 0;
+                        //ファイルを閉じて終了
+                        fclose(database);
+                        return 0; //なぜかbreakでうまくいかない
                     }
                 }
 
                 //終端文字を追加
                 id[SIZE_GID + 1] = '\0';
 
-
-
                 //GIDが一致する行があれば更新
                 if(strcmp(id, gid) == 0){
                     //カンマはgid,pin,name,birth,と4つ出てきている
                     const int num_canma = 4;
 
-                    //moneyの文字のところまで移動
-                    fseek(fp_write, num_canma + SIZE_PIN + (2 * SIZE_BODY_DATA), SEEK_CUR);
+                    //moneyの項目のところまで移動
+                    fseek(database, num_canma + SIZE_PIN + (2 * SIZE_BODY_DATA), SEEK_CUR);
+                    
+                    //moneyの更新（上書き）開始
                     for(int j = 0; j < SIZE_BODY_DATA; j++){
-                        //文字ずつ上書きしていく
-                        fputc(value[j], fp_write);
+                        // 1文字ずつ上書きしていく
+                        fputc(value[j], database);
                     }
-                    //アンダーバー、カンマ、改行文字を飛び越えたいのでfseekで3文字分の移動
-                    //データは85Byteずつ3つ来る。余る1Byteはnullじゃなくてアンダーバーで埋めてる
-                    fseek(fp_write, 3, SEEK_CUR);
+
+                    //上書き終了後
+                    /*
+                    　アンダーバー、カンマ、改行文字を飛び越えたいのでfseekで3文字分の移動
+                    　次の行の先頭に移動する
+                    　データは85Byteずつ3つ来る。余る1Byteはnullじゃなくてアンダーバーで埋めてる
+                    */
+                    fseek(database, 3, SEEK_CUR);
                 }
 
                 //GIDが一致していない場合、次の行に飛ぶ
                 else{
-                    fseek(fp_write, SIZE_DATA_LINE - SIZE_GID, SEEK_CUR);
+                    fseek(database, SIZE_DATA_LINE - SIZE_GID, SEEK_CUR);
                 }
-        
-                
-                
+
+                            
         }
-        fclose(fp_write);
+        //
+        fclose(database);
         return 0;
     }
 
@@ -95,15 +104,20 @@
     int main(void){
 
 
-        char value_update[SIZE_BODY_DATA];
+        char money_value[SIZE_BODY_DATA];
         const char* gid = "0000000001";
         const char* type = "money";
-        strcpy(value_update, "ABCDEFG");
+        strcpy(money_value, "43億8800万"); //漢字でも43.8800.0000でも大丈夫
 
+        Update("data3.txt", gid, type, money_value);
 
-        //Getが受け取る文字列は終端文字がきちんと含まれてる前提
-        Update("data3.txt", gid, type, value_update);
-
+        //テスト項目
+        //1.gid, typeなどに終端文字を含めずUpdateを呼び出した場合 : 今のままだとメモリ系のエラー(セグメンテーションフォールトとかコアダンプ)
+        //2.各項目に大きな値(SIZE_BODY_DATAより多い文字数) を入力した場合 : 上記と同じエラー(strcpyではなくstr「n」cpy使うとかで対策)
+        //3.データベースファイルを、存在しないファイルにしたらどうなるか　: エラー
+        //4.データベースにGIDとかの項目をあらかじめ書いていなかったら : 
+        //5.moneyの更新の値(money_value)の途中に「end」とか「'\0'」とか「'\n'」を入れたらどうなるか
+        //上記は、私がコーディング中に「これやられたらやばいな」と思ったもので、対策はしてません
 
     	return 0;
     }
--- common.h
+++ common.h
@@ -1,17 +1,22 @@
 #pragma once
 
+
+
+
+
+
+
+
+////////////////// クライアント - サーバー間通信 ////////////////////////////////
+
+//送受信のデータ量
+//header(2) + GID(10) + PIN(4) + body(256)
 #define MAX_BUFF_SIZE 272
-#define NUM_CANMA 5
-#define NUM_NEWLINE_CHAR 1
-//ヘッダーは含まない、カンマと改行文字
-#define SIZE_DATA_LINE (MAX_BUFF_SIZE - SIZE_HEADER + NUM_CANMA + NUM_NEWLINE_CHAR)
 
+//ヘッダー（要求を表す部分)が何バイト目か
 #define HEADER_COMMAND 0
+//ヘッダー (データの項目を表す部分)が何バイト目か
 #define HEADER_DATA_TYPE 1
-
-//const char* STR_NAME = "name";
-//const char* STR_BIRTH = "birth";
-//const char* STR_MONEY = "money";
 
 #define SIZE_HEADER  2
 #define SIZE_GID  10
@@ -29,9 +34,36 @@
 
 #define COMMAND_CLIENT_LOGOUT 4
 #define COMMAND_CLIENT_LOGIN 5
+//////////////////////////////////////////////////
 
 
+
+
+////////////////////// ログイン & モード ////////////////////////////////
+
+//ログインの可否
 #define COMMAND_SERVER_LOGIN_NG 0
 #define COMMAND_SERVER_LOGIN_OK 1
+
+//更新の可否
 #define COMMAND_SERVER_LOGIN_OK_RW 0
 #define COMMAND_SERVER_LOGIN_OK_RO 1
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////// データ管理 //////////////////////////////////////////
+
+//1行あたりのカンマの数
+#define NUM_CANMA 5
+
+//1行あたりの改行文字の個数
+#define NUM_NEWLINE_CHAR 1
+
+//データベースファイルの一行あたりの文字数 ヘッダーは含まない、カンマと改行文字をプラス
+#define SIZE_DATA_LINE (MAX_BUFF_SIZE - SIZE_HEADER + NUM_CANMA + NUM_NEWLINE_CHAR)
+
+///////////////////////////////////////////////////////////////////////
