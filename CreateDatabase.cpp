85eb4fb4a779d9e443073172c7e71f469b2e180b
Kazuma-Ogasa <knct0831@gmail.com>
2022-06-26 22:00:00 +0900

    refactor database processors

--- CreateDatabase.cpp
+++ CreateDatabase.cpp
@@ -13,8 +13,11 @@
 
         for(int i = 0; i < (SIZE_BODY_DATA + 1); i++){
 
-            if(i == SIZE_BODY_DATA){
+            if(i == SIZE_BODY_DATA && !needMatch256Byte){
                 buff[i] = ',';
+            }
+            else if(i == SIZE_BODY_DATA && needMatch256Byte){
+                buff[i] = '_';
             }
             else if(i < strlen(value)){
                 buff[i] = value[i];
@@ -25,7 +28,7 @@
         }
         if(needMatch256Byte){
             strncat(newline, buff, SIZE_BODY_DATA + 1);
-            strncat(newline, "_", 1);
+            strncat(newline, ",", 1);
             
         }
         else{
@@ -34,34 +37,23 @@
         
     }
 
-
-
-
-    int main(void){
-
-        FILE *fp;
-        char fname[] = "data3.txt";    
-        fp = fopen(fname, "r+");
-        char standardItems[SIZE_DATA_LINE];
+    void MakeHeaderLine(FILE* fp, char* standardItems){
+        //データベースの最初の行を作成する
+        
+        //項目とカンマを最初に文字列に追加
         strcpy(standardItems, "id,pin,name,birth,money");
+        //一行のデータ量は固定（固定長にしないと更新プログラムと合わなくなる)
         for(int i = strlen(standardItems); i < SIZE_DATA_LINE; i++){
-            standardItems[i] = ' ';
+            //何もないところは空白で埋める
+            standardItems[i] = ' '; 
         }
+        //カンマと改行
         standardItems[SIZE_DATA_LINE - 2] = ',';
         standardItems[SIZE_DATA_LINE - 1] = '\n';
-        fputs(standardItems, fp);
-        printf("%c\n", standardItems[SIZE_DATA_LINE - 2]);
-        printf("%c\n", standardItems[SIZE_DATA_LINE - 1 ]);
-        
+    
+    }
 
-        char newline[SIZE_DATA_LINE];
-
-        const char* gid = "1234567891";
-        const char* pin = "1234";
-        const char* name = "Kazuma";
-        const char* birth = "19990406";
-        const char* money = "123456789123456789";
-
+    void MakeUserDataLine(char* newline, const char* gid, const char* pin, const char* name, const char* birth, const char* money){
         strncat(newline, gid, SIZE_GID);
         strncat(newline, ",", 1);
         strncat(newline, pin, SIZE_PIN);
@@ -72,16 +64,58 @@
         AddData(newline, birth, false);
         AddData(newline, money, true);
 
-        
         strcat(newline, "\n");
-        fputs(newline, fp);
+    }
 
+
+    int main(void){
+
+        //ファイルの準備
+        FILE *fp;
+        //ファイルがない場合は自分の手で新規作成する必要があります
+        char fname[] = "data3.txt";    
+        fp = fopen(fname, "r+");
+
+        //最初の項目の行を生成
+        char standardItems[SIZE_DATA_LINE];
+        MakeHeaderLine(fp, standardItems);
+        fputs(standardItems, fp);
+
+
+        
+        //ユーザー情報の新しい行を追加する//////////////////
+        char newline[SIZE_DATA_LINE];
+        memset(newline, '\0', sizeof(newline));
+
+        const char* gid = "1234567891";
+        const char* pin = "1234";
+        const char* name = "Kazuma";
+        const char* birth = "19990406";
+        const char* money = "123456789123456789";
+
+        MakeUserDataLine(newline, gid, pin, name, birth, money);
+        fputs(newline, fp);
+        ///////////////////////////////////////////
+
+
+
+        //ユーザー2
+        char newline2[SIZE_DATA_LINE];
+        memset(newline2, '\0', sizeof(newline2));
+
+        const char* gid2 = "0000000001";
+        const char* pin2 = "0002";
+        const char* name2 = "Somen";
+        const char* birth2 = "20220622";
+        const char* money2 = "0010000100";
+
+        MakeUserDataLine(newline2, gid2, pin2, name2, birth2, money2);
+        fputs(newline2, fp);
+
+
+
+        //ファイルの最後　ここを変更すると更新プログラムと合わなくなる
         fputs("end\n", fp);
     
-
-
-
-
-
     	return 0;
     }
--- DataController.cpp
+++ DataController.cpp
@@ -28,23 +28,25 @@
 
 
 
-    int Update(const char* gid, const char* type, char* value){
+    int Update(const char* dataFileName, const char* gid, const char* type, char* value){
 
             FILE *fp_write;
-            char fname_write[] = "data3.txt";
             
-            fp_write = fopen(fname_write, "r+");
+            //r+ は読み書き両方のモード
+            fp_write = fopen(dataFileName, "r+");
 
             while(1){
 
-                
+                //id読み取り用配列（一応毎回初期化する)    
                 char id[SIZE_GID + 1];
                 memset(id, '\0', sizeof(id));
 
                 //ファイルからGIDを取得
                 for(int i = 0; i < SIZE_GID; i++){
                     id[i] = fgetc(fp_write);
-                //終了判定 
+
+                //終了判定 データベースの最終行は「end」の文字列
+                //ゴリ押しif文感満載
                     if(id[0] == 'e' && id[1] == 'n' && id[2] == 'd'){
                         printf("end");
                         return 0;
@@ -58,14 +60,21 @@
 
                 //GIDが一致する行があれば更新
                 if(strcmp(id, gid) == 0){
-                    fseek(fp_write, 4 + SIZE_PIN + (2 * SIZE_BODY_DATA), SEEK_CUR);
+                    //カンマはgid,pin,name,birth,と4つ出てきている
+                    const int num_canma = 4;
+
+                    //moneyの文字のところまで移動
+                    fseek(fp_write, num_canma + SIZE_PIN + (2 * SIZE_BODY_DATA), SEEK_CUR);
                     for(int j = 0; j < SIZE_BODY_DATA; j++){
+                        //文字ずつ上書きしていく
                         fputc(value[j], fp_write);
                     }
-                    fputc(',', fp_write);
-                    fputc('\n', fp_write);
-                    fseek(fp_write, 1, SEEK_CUR);
+                    //アンダーバー、カンマ、改行文字を飛び越えたいのでfseekで3文字分の移動
+                    //データは85Byteずつ3つ来る。余る1Byteはnullじゃなくてアンダーバーで埋めてる
+                    fseek(fp_write, 3, SEEK_CUR);
                 }
+
+                //GIDが一致していない場合、次の行に飛ぶ
                 else{
                     fseek(fp_write, SIZE_DATA_LINE - SIZE_GID, SEEK_CUR);
                 }
@@ -87,13 +96,13 @@
 
 
         char value_update[SIZE_BODY_DATA];
-        const char* gid = "1234567891";
+        const char* gid = "0000000001";
         const char* type = "money";
         strcpy(value_update, "ABCDEFG");
 
 
         //Getが受け取る文字列は終端文字がきちんと含まれてる前提
-        Update(gid, type, value_update);
+        Update("data3.txt", gid, type, value_update);
 
 
     	return 0;
